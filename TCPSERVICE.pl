5e140f298dbb2f136691b8623c74af85be4eda8e
Theo Schlossnagle <jesus@omniti.com>
2001-09-20 06:05:29 +0000

    prepost and non ASCII banners
    git-svn-id: https://labs.omniti.com/resmon/trunk@7 8c0face9-b7db-6ec6-c4b3-d5f7145c7d55

--- TCPSERVICE.pl
+++ TCPSERVICE.pl
@@ -18,6 +18,7 @@ sub {
   my $handle = new IO::Handle;
   socket($handle, Socket::PF_INET, Socket::SOCK_STREAM, $proto) ||
     return "BAD(socket error)";
+  $handle->autoflush(1);
   fcntl($handle, Fcntl::F_SETFL, Fcntl::O_NONBLOCK) ||
     (close($handle) && return "BAD(fcntl error)");
   my $sin = Socket::sockaddr_in($port, Socket::inet_aton($host));
@@ -31,12 +32,14 @@ sub {
       close($handle);
       return "BAD(connect failed)";
     }
+    print $handle $arg->{prepost}."\r\n" if ($arg->{prepost});
     ($fd) = $con->can_read($timeout);
     if($fd == $handle) {
       my $banner;
       chomp($banner = <$handle>);
       print $handle $arg->{post} if ($arg->{post});
       close($handle);
+      $banner =~ s/([^\s\d\w.,;\/\\])/sprintf "\\%o", $1/eg;
       return "BAD($banner)"
         if($arg->{match} && ($banner =! /$arg->{match}/));
       return "OK($banner)";
