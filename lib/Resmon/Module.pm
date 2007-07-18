a7d2d7beb82bb785cdd2982a653f279494d3530a
Eric Sproul <esproul@omniti.com>
2007-07-18 17:19:55 +0000

    Fix a regex bug with remoteport match, update regexes to work on Linux as well as Solaris
    git-svn-id: https://labs.omniti.com/resmon/trunk@43 8c0face9-b7db-6ec6-c4b3-d5f7145c7d55

--- lib/Resmon/Module.pm
+++ lib/Resmon/Module.pm
@@ -191,12 +191,12 @@ sub handler {
   return $os if $os;
   my $output = cache_command("netstat -an", 30);
   my @lines = split(/\n/, $output);
-  @lines = grep(/\s$arg->{state}$/, @lines) if($arg->{state});
+  @lines = grep(/\s$arg->{state}\s*$/, @lines) if($arg->{state});
   @lines = grep(/^$arg->{localip}/, @lines) if($arg->{localip});
-  @lines = grep(/^[\d\*\.]+\.$arg->{localport}/, @lines) if($arg->{localport});
-  @lines = grep(/^[\d\*\.]+\d+\s+$arg->{remoteip}/, @lines)
+  @lines = grep(/^[\w\d\*\.]+.*[\.\:]+$arg->{localport}/, @lines) if($arg->{localport});
+  @lines = grep(/[\d\*\.]+\d+\s+$arg->{remoteip}/, @lines)
 	if($arg->{remoteip});
-  @lines = grep(/^[\d\*\.]+\s+[\d\*\.+]\.$arg->{remoteport}/, @lines)
+  @lines = grep(/[\d\*\.]+\s+[\d\*\.]+[\.\:]+$arg->{remoteport}\s+/, @lines)
 	if($arg->{remoteport});
   my $count = scalar(@lines);
   return $arg->set_status("BAD($count)")
