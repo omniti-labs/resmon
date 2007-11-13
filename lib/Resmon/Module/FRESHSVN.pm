4166e2036e68c43be4f3cb344a12336fa249d728
Sergey Ivanov <seriv@omniti.com>
2007-11-13 19:56:05 +0000

    fixes
    git-svn-id: https://labs.omniti.com/resmon/trunk@51 8c0face9-b7db-6ec6-c4b3-d5f7145c7d55

--- lib/Resmon/Module/FRESHSVN.pm
+++ lib/Resmon/Module/FRESHSVN.pm
@@ -10,27 +10,37 @@ sub handler {
   return $os if $os;
   my $dir = $arg->{'object'};
   my $moutput = cache_command("/opt/omni/bin/svn info $dir", 60);
-  my @molines = split (/\n/,$moutput);
-  my ($URL,$mr,$mY,$mM,$mD,$mh,$mm,$ms);
-  for(@molines) {
+  my @mlines = split (/\n/,$moutput);
+  my ($URL,$mr);
+  for(@mlines) {
     if (/^URL:\s*(.*)$/) { $URL=$1; }
-    elsif (/^Last Changed Rev:\s*(\d+)/) { $mr = $1; }
-    elsif (/^Last Changed Date:\s*(\d{4})-(\d{1,2})-(\d{1,2}) (\d{1,2}):(\d{2}):(\d{2})/) {
-      ($mY,$mM,$mD,$mh,$mm,$ms) = ($1,$2,$3,$4,$5,$6);
-    }
+    elsif (/^Revision:\s*(\d+)/) { $mr = $1; }
   }
   return ($arg->set_status("BAD(wrong URL, in conf:".$arg->{'URL'}.", checked out: $URL)")) if ($URL ne $arg->{'URL'});
-  my $uoutput = cache_command("/opt/omni/bin/svn info --username svnsync --password Athi3izo  --no-auth-cache --non-interactive $arg->{'URL'} 2>&1 ", 60);
+  my $uoutput = cache_command("/opt/omni/bin/svn info --username svnsync --password Athi3izo  --no-auth-cache --non-interactive $URL", 60);
   my @ulines = split (/\n/,$uoutput);
-  my ($ur,$uY,$uM,$uD,$uh,$um,$us);
+  my ($ur);
   for(@ulines) {
-    if (/^Last Changed Rev:\s*(\d+)/) { $ur = $1; }
-    elsif (/^Last Changed Date:\s*(\d{4})-(\d{1,2})-(\d{1,2}) (\d{1,2}):(\d{2}):(\d{2})/) {
-      ($uY,$uM,$uD,$uh,$um,$us) = ($1,$2,$3,$4,$5,$6);
-    }
+    if (/^Revision:\s*(\d+)/) { $ur = $1; }
   }
   if($ur == $mr){ return($arg->set_status("OK(rev:$ur)")); }
   else{
+    my ($uY,$uM,$uD,$uh,$um,$us);
+    for(@ulines) {
+      if (/^Last Changed Date:\s*(\d{4})-(\d{1,2})-(\d{1,2}) (\d{1,2}):(\d{2}):(\d{2})/) {
+        ($uY,$uM,$uD,$uh,$um,$us) = ($1,$2,$3,$4,$5,$6);
+         print;
+      }
+    }
+    my $routput = cache_command("/opt/omni/bin/svn info --username svnsync --password Athi3izo  --no-auth-cache --non-interactive $URL\@$mr", 60);
+    my @rlines = split (/\n/,$routput);
+    my ($mY,$mM,$mD,$mh,$mm,$ms);
+    for(@rlines) {
+       if (/^Last Changed Date:\s*(\d{4})-(\d{1,2})-(\d{1,2}) (\d{1,2}):(\d{2}):(\d{2})/) {
+         ($mY,$mM,$mD,$mh,$mm,$ms) = ($1,$2,$3,$4,$5,$6);
+         print;
+       }
+    }
     my ($mTime,$uTime,$lag,$maxlag);
     $mTime=$ms+60*($mm+60*($mh+24*($mD+31*($mM+12*$mY))));
     $uTime=$us+60*($um+60*($uh+24*($uD+31*($uM+12*$uY))));
