bf11e9eb11e24e5675b3a6a4eb98b9235c4d2ed4
Keri Johnson <keri@omniti.com>
2007-11-28 22:56:15 +0000

    nifty hack for working this on solaris by theo
    git-svn-id: https://labs.omniti.com/resmon/trunk@61 8c0face9-b7db-6ec6-c4b3-d5f7145c7d55

--- lib/Resmon/Module/INODES.pm
+++ lib/Resmon/Module/INODES.pm
@@ -1,15 +1,19 @@
+keri@ngs-va-01[/opt/resmon/lib/Resmon/Module]$ sudo svcadm refresh resmon
+keri@ngs-va-01[/opt/resmon/lib/Resmon/Module]$ more INODES.pm 
 package Resmon::Module::INODES;
 use Resmon::ExtComm qw/cache_command/;
 use Resmon::Module;
 use vars qw/@ISA/;
 @ISA = qw/Resmon::Module/;
 
+my $dfcmd = ($^O eq 'solaris') ? 'df -Fufs -oi' : 'df -i';
+
 sub handler {
   my $arg = shift;
   my $os = $arg->fresh_status();
   return $os if $os;
   my $devorpart = $arg->{'object'};
-  my $output = cache_command("df -i", 30);
+  my $output = cache_command($dfcmd, 30);
   my ($line) = grep(/$devorpart\s*/, split(/\n/, $output));
   if($line =~ /(\d+)\s+(\d+)\s+(\d+)\s+(\d+)%/) {
     if($4 <= $arg->{'limit'}) {
@@ -21,3 +25,4 @@ sub handler {
 }
 
 1;
+
