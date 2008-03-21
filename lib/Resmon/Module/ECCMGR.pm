ac8ea321f2bf083755630d7127e4eef230a5c183
Mark Harrison <mark@omniti.com>
2008-03-21 15:49:59 +0000

    Adding Ecelerity Cluster Manager check for resmon
    git-svn-id: https://labs.omniti.com/resmon/trunk@72 8c0face9-b7db-6ec6-c4b3-d5f7145c7d55

---
+++ lib/Resmon/Module/ECCMGR.pm
@@ -0,0 +1,43 @@
+package Resmon::Module::ECCMGR;
+use Resmon::ExtComm qw/cache_command/;
+use vars qw/@ISA/;
+@ISA = qw/Resmon::Module/;
+
+# Note: This requires you have access to the ecelerity control lib. The
+# easiest way to do this is to start resmon using ecelerity's perl install.
+use Ecelerity::Control;
+
+# Sample config for resmon.conf
+# (The module doesn't actually use anything other than 'services' to use as
+#  a label)
+# ECCMGR {
+#   eccmgr : socket => /tmp/2026
+# }
+
+sub handler {
+  my $arg = shift;
+  my $os = $arg->fresh_status();
+  return $os if $os;
+  my $proc = $arg->{'object'};
+  my $socket = $arg->{'socket'} || '/tmp/2026';
+  # Connect to ecelerity
+  my $ec = Ecelerity::Control->new({"Control" => $socket});
+  eval {
+    # Check the version
+    $version = $ec->command('version');
+  };
+  if ($@) {
+      # Catch any could not connect error
+      $@ =~ /^(.*) at/; # Fetch just the error message and no file/line no.
+      return $arg->set_status("BAD($1)");
+  }
+  if ($version =~ /(eccmgr version: .*)\n/) {
+      return $arg->set_status("OK($1)");
+  } else {
+      # Something other than eccmgr responded, print out a version string
+      $version =~ s/\n/ /g;
+      $version =~ s/#//g;
+      return $arg->set_status("BAD(eccmgr not running: $version)");
+  }
+};
+
