f40727423e447fc98b5a0bd521e879700cedf78f
Mark Harrison <mark@omniti.com>
2008-05-19 17:46:25 +0000

    Adding updated FILECOUNT check used in bluestreak ionmx monitoring.
    git-svn-id: https://labs.omniti.com/resmon/trunk@98 8c0face9-b7db-6ec6-c4b3-d5f7145c7d55

---
+++ lib/Resmon/Module/FILECOUNT.pm
@@ -0,0 +1,24 @@
+package Resmon::Module::FILECOUNT;
+use Resmon::ExtComm qw/cache_command/;
+use vars qw/@ISA/;
+@ISA = qw/Resmon::Module/;
+
+sub handler {
+        my $arg = shift;
+        my $os = $arg->fresh_status();
+        return $os if $os;
+        my $dir = $arg->{'object'};
+        my $hlimit = $arg->{'hard_limit'};
+        my $slimit = $arg->{'soft_limit'};
+        my $output = cache_command("ls $dir | wc -l",30);
+        chomp($output);
+        if ($output > $hlimit) {
+                return "BAD", "$output files";
+        } elsif ($output > $slimit) {
+                return "WARNING", "$output files";
+        } else {
+                return "OK", "$output files";
+        }
+}
+
+1;
