c9675cb5dfd3dc82a465d4cdc92b6f861474561e
Mark Harrison <mark@omniti.com>
2008-05-21 19:01:09 +0000

    Fixed a bug in the resmon module and added checking for bad config/failed module loading.
    git-svn-id: https://labs.omniti.com/resmon/trunk@105 8c0face9-b7db-6ec6-c4b3-d5f7145c7d55

--- lib/Resmon/Module/RESMON.pm
+++ lib/Resmon/Module/RESMON.pm
@@ -10,11 +10,17 @@ use Sys::Hostname;
 # returning OK. In future it will check for a broken config and broken
 # modules, reporting that it is running with old modules/an old config.
 
-(my $resmon_dir = $0) =~ s/\/?[^\/]+$//;
+my $resmon_dir = $0;
+$resmon_dir =~ s/\/?[^\/]+$//;
 
 sub handler {
     my $arg = shift;
     my $os = $arg->fresh_status();
+
+    # Get the global config object
+    my $config = $main::config;
+    my $configstatus = $config->{'configstatus'} || "";
+    my $modstatus = $config->{'modstatus'} || "";
 
     ## Current revision
     # Find location of subversion binary
@@ -25,8 +31,8 @@ sub handler {
             last;
         }
     }
-    my $output = cache_command("$svn info $resmon_path 2>&1", 600);
-    $revision = 0;
+    my $output = cache_command("$svn info $resmon_dir 2>&1", 600);
+    my $revision = 0;
     for (split(/\n/, $output)) {
         if (/^Revision:\s*(\d*)$/) { $revision = "r$1"; }
         if (/^svn: (.*) is not a working copy$/) {
@@ -38,11 +44,24 @@ sub handler {
     # The hostname command croaks (dies) if it fails, hence the eval
     my $hostname = eval { hostname } || "Unknown";
 
+    my $status = "OK";
+    my $statusmsg = "running";
+    if ($configstatus) {
+        $statusmsg = "BAD config file, running on old configuration";
+        $status = "BAD";
+    }
+    if ($modstatus) {
+        $statusmsg .= " with failed modules: $modstatus";
+        $status = "BAD";
+    }
+
     # Set 'config' variables so it shows up in the xml output
     $arg->{'revision'} = $revision;
     $arg->{'hostname'} = $hostname;
+    $arg->{'configstatus'} = $configstatus || "OK";
+    $arg->{'modstatus'} = $modstatus || "OK";
 
-    return $arg->set_status("OK", "$hostname running ($revision)");
+    return $arg->set_status($status, "$hostname $statusmsg ($revision)");
 }
 
 1;
