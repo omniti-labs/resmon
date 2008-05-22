a3ac1576f50568b5809883c6079ff8398ef2eff5
Mark Harrison <mark@omniti.com>
2008-05-22 19:36:52 +0000

    Added a purge method to status that will remove any status information for modules that are no longer loaded (due to them being removed from the config file and resmon being reloaded). This required $status to become global so it was accessible from the reload_modules command.
    git-svn-id: https://labs.omniti.com/resmon/trunk@109 8c0face9-b7db-6ec6-c4b3-d5f7145c7d55

--- lib/Resmon/Status.pm
+++ lib/Resmon/Status.pm
@@ -404,6 +404,42 @@ sub store {
     print "$name($type) :: $info->{state}($info->{message})\n";
   }
 }
+sub purge {
+    # This removes status information for modules that are no longer loaded
+
+    # Generate list of current modules
+    my %loaded = ();
+    my ($self, $config) = @_;
+    while (my ($type, $mods) = each(%{$config->{Module}}) ) {
+        $loaded{$type} = ();
+        foreach (@$mods) {
+            $loaded{$type}{$_->{'object'}} = 1;
+        }
+    }
+
+    # Debugging
+    #while (my ($key, $value) = each(%loaded) ) {
+    #    print STDERR "$key: ";
+    #    while (my ($mod, $dummy) = each (%$value) ) {
+    #        print STDERR "$mod ";
+    #    }
+    #    print "\n";
+    #}
+
+    # Compare $self->{store} with list of loaded modules
+    while (my ($type, $value) = each (%{$self->{store}})) {
+        while (my ($name, $value2) = each (%$value)) {
+            if (!exists($loaded{$type}) || !exists($loaded{$type}{$name})) {
+                print STDERR "$type $name\n";
+                delete $self->{store}->{$type}->{$name};
+                if (scalar(keys %{$self->{store}->{$type}}) == 0) {
+                    print STDERR "$type has no more objects, deleting\n";
+                    delete $self->{store}->{$type};
+                }
+            }
+        }
+    }
+}
 sub close {
   my $self = shift;
   return if($self->{handle_is_stdout});
