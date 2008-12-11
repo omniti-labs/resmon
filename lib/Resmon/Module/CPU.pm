ac12e51369119edd57d0699062ab00ae200ac562
Mark Harrison <mark@omniti.com>
2008-12-11 16:59:06 +0000

    Solaris/BSD support + show individual user/sys/wait times in the output
    git-svn-id: https://labs.omniti.com/resmon/trunk@152 8c0face9-b7db-6ec6-c4b3-d5f7145c7d55

--- lib/Resmon/Module/CPU.pm
+++ lib/Resmon/Module/CPU.pm
@@ -23,27 +23,39 @@ sub handler {
     $cpuuser = 0;
     $cpusys  = 0;
     $cpuwait = 0;
+    $havewait = 0;
 
     $fieldcount = @headers;
+    # Look at the field header names to determine which values to use
     for (my $i=0; $i < $fieldcount; $i++) {
-        if ($headers[$i] eq 'us') {
+        # Need to compare previous/next header because solaris has two 'sy'
+        # values - one for cpu and one for interrupts
+        if ($headers[$i] eq 'us' && $headers[$i+1] eq 'sy') {
             $cpuuser = $vals[$i];
         }
-        if ($headers[$i] eq 'sy') {
+        if ($headers[$i] eq 'sy' && $headers[$i-1] eq 'us') {
             $cpusys = $vals[$i];
         }
+        # Wait appears to be a linux thing, so on other platforms it will just
+        # be 0
         if ($headers[$i] eq 'wa') {
             $cpuwait = $vals[$i];
+            $havewait = 1;
         }
     }
 
     #print "$cpuuser, $cpusys, $cpuwait\n";
     $total = $cpuuser + $cpusys + $cpuwait;
 
+    $summary = "- us:$cpuuser sy:$cpusys";
+    if ($havewait) {
+        $summary = "$summary wa:$cpuwait";
+    }
+
     if ($total > $limit) {
-        return "BAD", "$total > $limit";
+        return "BAD", "$total > $limit $summary";
     } else {
-        return "OK", "$total <= $limit";
+        return "OK", "$total <= $limit $summary";
     }
 
     return "BAD", "Unable to determine cpu info";
