6906447522cf87b345c787f46883eb3c1503cd83
Mark Harrison <mark@omniti.com>
2009-03-24 15:11:27 +0000

    Treat a zpool that needs an upgrade as WARNING or OK (configurable
    git-svn-id: https://labs.omniti.com/resmon/trunk@164 8c0face9-b7db-6ec6-c4b3-d5f7145c7d55

--- lib/Resmon/Module/ZPOOLERRS.pm
+++ lib/Resmon/Module/ZPOOLERRS.pm
@@ -10,19 +10,28 @@ use vars qw/@ISA/;
 
 # Sample configuration:
 # ZPOOLERRS {
+#   zpools : warn_on_upgrade => yes
+# }
+
+# Another sample configuration, which ignores zpool upgrade requests
+# ZPOOLERRS {
 #   zpools : noop
 # }
 
 sub handler {
     my $arg = shift;
     my $unit = $arg->{'object'};
+    # Do we warn on 'zpool needs an upgrade', or just leave it OK?
+    my $warn_on_upgrade = $arg->{'warn_on_upgrade'} || "no";
     my $output = cache_command(
         "zpool status -x", 600);
     if($output) {
-            my $errstring = "";
+            my %errs = ();
             my $currpool = "";
+            my $status = "OK";
             foreach my $line (split(/\n/, $output)) {
                 if ($line =~ /(all pools are healthy)/) {
+                    # If everything is OK, we don't need to go any further
                     return "OK", $1;
                 }
                 if ($line =~ /pool: (.+)$/) {
@@ -30,17 +39,39 @@ sub handler {
                     $currpool = $1;
                 } elsif ($line =~ /state: (.+)$/) {
                     # Pool state
-                    $errstring .= "$currpool:$1 ";
+                    $errs->{$currpool} .= "$1 ";
+                    if ($1 ne 'ONLINE') {
+                        $status = "BAD";
+                    }
+                } elsif ($line =~
+                    /The pool is formatted using an older on-disk format/) {
+                    # Detect if the error is just that a pool needs upgrading.
+                    $errs->{$currpool} .= "- needs upgrade ";
+                    if ($warn_on_upgrade eq "yes" && $status != "BAD") {
+                        $status = "WARNING";
+                    }
                 } elsif ($line =~
                     /([a-z0-9]+)\s+([A-Z]+)\s+(\d+)\s+(\d+)\s+(\d+)/) {
                     # Pool errors
                     if ($3 != 0 || $4 != 0 || $5 != 0) {
-                        $errstring .= "- $1:$2 w/Errs: R:$3 W:$4 Chk:$5 ";
+                        $errs->{$currpool} .=
+                            "- $1:$2 w/Errs: R:$3 W:$4 Chk:$5 ";
+                        $status = "BAD";
                     }
                 }
             }
+            # Generate the status
+            my $errstring = "";
+            while (my ($k, $v) = each %$errs) {
+                # $v should always contain a trailing space
+                chop($v);
+                $errstring .= "$k: $v";
+                $errstring .= ", ";
+            }
+            # Remove the trailing comma and space
             chop($errstring);
-            return "BAD", $errstring;
+            chop($errstring);
+            return $status, $errstring;
     }
     return "BAD", "no data"
 };
