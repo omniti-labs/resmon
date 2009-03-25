323fb1414e6da2981f5fae666393e958ab6a4d39
Mark Harrison <mark@omniti.com>
2009-03-25 15:30:51 +0000

    We shouldn't need to grep -v sh in the pgrep command
    git-svn-id: https://labs.omniti.com/resmon/trunk@174 8c0face9-b7db-6ec6-c4b3-d5f7145c7d55

--- lib/Resmon/Module/PGREP.pm
+++ lib/Resmon/Module/PGREP.pm
@@ -9,7 +9,7 @@ sub handler {
   my $args = join(' ',$arg->{'arg0'},$arg->{'arg1'},$arg->{'arg2'});
   $args =~s/\s+$//;
   $proc .= " $args" if $args;
-  my $output = cache_command("pgrep -f -l '$proc' | grep -v sh | head -1", 500);
+  my $output = cache_command("pgrep -f -l '$proc' | head -1", 500);
   if($output) {
     chomp $output;
     return("OK(pid:$output)");
