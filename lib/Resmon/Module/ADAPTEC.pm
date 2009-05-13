2d1cb93ed8a444ea6c5e1c801a9bccafa01a1951
Mark Harrison <mark@omniti.com>
2009-05-13 15:47:05 +0000

    Adaptec RAID module
    git-svn-id: https://labs.omniti.com/resmon/trunk@193 8c0face9-b7db-6ec6-c4b3-d5f7145c7d55

---
+++ lib/Resmon/Module/ADAPTEC.pm
@@ -0,0 +1,44 @@
+package Resmon::Module::ADAPTEC;
+use Resmon::ExtComm qw/cache_command/;
+use vars qw/@ISA/;
+use strict;
+@ISA = qw/Resmon::Module/;
+
+# Adaptec RAID status
+# requires the arcconf program to be installed (defaults to /usr/StorEdge, but
+# you can specify this in the config variable arcconf.
+#
+# Example config:
+# ADAPTEC {
+#   1 : noop
+# }
+#
+# ADAPTEC {
+#   1 : arcconf => /usr/StorMan/arcconf
+# }
+
+sub handler {
+    my $arg = shift;
+    # Unit number
+    my $unit = $arg->{'object'};
+    # Path to arcconf program
+    my $arcconf = $arg->{'arcconf'} || '/use/StorMan/arcconf';
+
+    my $status = "OK";
+    my $message;
+
+    my $output = cache_command("$arcconf getconfig $unit AD", 500);
+    foreach (split(/\n/, $output)) {
+        if (/Logical devices\/Failed\/Degraded\s+:\s+\d+\/(\d+)\/(\d+)/) {
+            my $failed = $1;
+            my $degraded = $2;
+            if ($failed > 0 || $degraded > 0) {
+                $status = "BAD";
+            }
+            $message = "$failed Failed, $degraded Degraded";
+            last;
+        }
+    }
+    return $status, $message;
+};
+1;
