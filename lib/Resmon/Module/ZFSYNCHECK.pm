c1ff1c12aa13b7dcd7348d37f330d63126759343
Jason Dixon <jdixon@omniti.com>
2009-11-11 01:02:51 +0000

    pass in age as seconds, not days
    git-svn-id: https://labs.omniti.com/resmon/trunk@228 8c0face9-b7db-6ec6-c4b3-d5f7145c7d55

--- lib/Resmon/Module/ZFSYNCHECK.pm
+++ lib/Resmon/Module/ZFSYNCHECK.pm
@@ -7,7 +7,7 @@ use Time::Local;
 sub handler {
   my $arg=shift;
   my $zfs=$arg->{'object'};
-  my $age=$arg->{'age'}*86400;
+  my $age=$arg->{'age'};
   my $recentsnap = cache_command("zfs list -tsnapshot -H -Screation -oname| grep '^$zfs\@' | head -1", 300);
   return "BAD(no snapshot of $zfs)" if not $recentsnap;
   my $snaptime = cache_command("zfs get -H -p -ovalue creation $recentsnap", 300);
