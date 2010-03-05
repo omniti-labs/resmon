6978dd342cb76b06a08cee1ee35b17b743cb66f1
Jason Dixon <jdixon@omniti.com>
2010-03-05 01:29:59 +0000

    prefer to fail check than to add hanging zfs request
    git-svn-id: https://labs.omniti.com/resmon/trunk@250 8c0face9-b7db-6ec6-c4b3-d5f7145c7d55

--- lib/Resmon/Module/ZFSYNCHECK.pm
+++ lib/Resmon/Module/ZFSYNCHECK.pm
@@ -8,7 +8,7 @@ sub handler {
   my $arg=shift;
   my $zfs=$arg->{'object'};
   my $age=$arg->{'age'};
-  my $recentsnap = cache_command("zfs list -tsnapshot -H -Screation -oname| grep '^$zfs\@' | head -1", 300);
+  my $recentsnap = cache_command("pgrep zfs 2>&1 >/dev/null || zfs list -tsnapshot -H -Screation -oname | grep '^$zfs\@' | head -1", 300);
   return "BAD(no snapshot of $zfs)" if not $recentsnap;
   my $snaptime = cache_command("zfs get -H -p -ovalue creation $recentsnap", 300);
   my $snapage=time()-$snaptime;
