f21a9bbe574ee86efd04eca84e06053eb4525e91
Jason Dixon <jdixon@omniti.com>
2010-04-25 21:27:07 +0000

    solaris support
    git-svn-id: https://labs.omniti.com/resmon/branches/resmon2@327 8c0face9-b7db-6ec6-c4b3-d5f7145c7d55

--- lib/Core/Memstat.pm
+++ lib/Core/Memstat.pm
@@ -38,6 +38,10 @@ respective vmstat command.
 The check name is used for descriptive purposes only.  It is not used
 for anything functional.
 
+=head1 METRICS
+
+=over
+
 =item actv_mem
 
 Active virtual pages.
@@ -46,19 +50,12 @@ Active virtual pages.
 
 Free real memory.
 
-=back
+=head1 KSTAT METRICS
 
-=head1 METRICS
-
-=over
-
-=item read_sec
-
-Reads per second.
-
-=item write_sec
-
-Writes per second.
+Solaris provides an interface to numerous kernel statistics.  If the
+Perl Sun::Solaris::Kstat library is locally available, this module will
+prefer that method first, bypassing vmstat collection.  Otherwise, this
+module falls back on the standard vmstat collection method.
 
 =back
 
@@ -71,7 +68,36 @@ sub handler {
     my $vmstat_path = $config->{'vmstat_path'} || 'vmstat';
     my $osname = $^O;
 
-    if ($osname eq 'openbsd') {
+    if ($osname eq 'solaris') {
+        my $usekstat = 0;
+        my $pagesize = run_command('pagesize');
+        my $kstat;
+        eval "use Sun::Solaris::Kstat";
+        unless ($@) {
+            $usekstat = 1;
+            $kstat = Sun::Solaris::Kstat->new();
+        }
+        if ($usekstat && $pagesize) {
+            my %metrics;
+            my $syspages = $kstat->{'unix'}->{0}->{'system_pages'};
+
+            foreach (keys %$syspages) {
+                $metrics{"kstat_${_}"} = [int($syspages->{$_} * $pagesize / 1024), 'i'] unless ($_ eq 'class');
+            }
+            $metrics{'kstat_cache_mem'} = int($kstat->{'zfs'}->{0}->{'arcstats'}->{'size'} / 1024);
+            return \%metrics;
+        } else {
+            my $output = run_command("$vmstat_path");
+            if ($output =~ /.*cs\s+us\s+sy\s+id\n\s+\d+\s+\d+\s+\d+\s+(\d+)\s+(\d+).*/) {
+                return {
+                    'actv_mem' => [$1, 'i'],
+                    'free_mem' => [$2, 'i']
+                };
+            } else {
+                die "Unable to extract statistics\n";
+            }
+        }
+    } elsif ($osname eq 'openbsd') {
         my $output = run_command("$vmstat_path");
         if ($output =~ /.*cs\s+us\s+sy\s+id\n\s+\d+\s+\d+\s+\d+\s+(\d+)\s+(\d+).*/) {
             return {
