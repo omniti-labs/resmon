1bd136a1a44a9c54126723b84bb9043cccec9b72
Jason Dixon <jdixon@omniti.com>
2010-05-06 02:43:41 +0000

    new module for pf queues
    git-svn-id: https://labs.omniti.com/resmon/branches/resmon2@352 8c0face9-b7db-6ec6-c4b3-d5f7145c7d55

---
+++ lib/Core/Pf/Queues.pm
@@ -0,0 +1,83 @@
+package Core::Pf::Queues;
+
+use strict;
+use warnings;
+
+use base 'Resmon::Module';
+
+use Resmon::ExtComm qw(run_command);
+use Data::Dumper;
+
+=pod
+
+=head1 NAME
+
+Core::Pf::Queues - gather queue statistics from PF firewalls
+
+=head1 SYNOPSIS
+
+ Core::Pf::Queues {
+    local : pfctl_path => /sbin/pfctl
+ }
+
+=head1 DESCRIPTION
+
+This module retrieves queue statistics from PF firewalls using
+the pfctl command.  Each metric returned is prefixed with the
+name of the associated queue.
+
+=head1 CONFIGURATION
+
+=over
+
+=item check_name
+
+Arbitrary name of the check.
+
+=item pfctl_path
+
+Optional path to the pfctl executable.
+
+=back
+
+=head1 METRICS
+
+=over
+
+=item bytes
+
+=item pkts
+
+=item drop_bytes
+
+=item drop_pkts
+
+=back
+
+=cut
+
+sub handler {
+    my $self = shift;
+    my $config = $self->{'config'};
+    my $pfctl_path = $config->{'pfctl_path'} || 'pfctl';
+    my $output = run_command("$pfctl_path -vsq");
+    my $osname = $^O;
+    my %metrics;
+
+    if ($osname eq 'openbsd') {
+        foreach (split(/queue\s+/, $output)) {
+            next unless /\w+/;
+            /(\S+)\s+.*\n\s+\[\s+pkts\:\s+(\d+)\s+bytes\:\s+(\d+)\s+dropped\s+pkts\:\s+(\d+)\s+bytes\:\s+(\d+).*/;
+            $metrics{"${1}_pkts"} = [$2, 'i'],
+            $metrics{"${1}_bytes"} = [$3, 'i'],
+            $metrics{"${1}_drop_pkts"} = [$4, 'i'],
+            $metrics{"${1}_drop_bytes"} = [$5, 'i'],
+        }
+    } else {
+        die "Unknown platform: $osname";
+    }
+
+    return { %metrics };
+};
+
+1;
