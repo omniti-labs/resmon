8ae43b1e393082c20dc02a5a3998b1abb01a9fd7
Eric Sproul <esproul@omniti.com>
2011-01-29 19:28:34 +0000

    Core::BSD::Sensor for reading hardware sensors under OpenBSD (and maybe other BSDs as well.)  Based on CHECK_BSD_TEMP from resmon1.
    git-svn-id: https://labs.omniti.com/resmon/trunk@434 8c0face9-b7db-6ec6-c4b3-d5f7145c7d55

---
+++ lib/Core/BSD/Sensor.pm
@@ -0,0 +1,77 @@
+package Core::BSD::Sensor;
+
+use strict;
+use warnings;
+
+use base 'Resmon::Module';
+
+use Resmon::ExtComm qw(run_command cache_command);
+
+=pod
+
+=head1 NAME
+
+Core::BSD::Sensor - Check values of OpenBSD hardware sensors
+
+=head1 SYNOPSIS
+
+ Core::BSD::Sensor {
+     cpu_temp: chip => admtm0, sensor => temp1
+     sys_temp: chip => admtm0, sensor => temp2
+     cpu_fan: chip => fins0, sensor => fan0
+ }
+
+=head1 DESCRIPTION
+
+This module reads values from hardware sensors using sysctl on OpenBSD.
+
+=head1 CONFIGURATION
+
+=over
+
+=item check_name
+
+The check name is a short description of what the particular sensor monitors.
+
+=item chip
+
+Specifies the desired sensor chip.  This argument is mandatory.
+
+=item sensor
+
+The sensor on the specified chip whose value should be read.  This argument is mandatory.
+
+=back
+
+=head1 METRICS
+
+=over
+
+=item value
+
+The value of the specified sensor.
+
+=back
+
+=cut
+
+sub handler {
+    my $self = shift;
+    my $config = $self->{config}; # All configuration is in here
+    my $check_name = $self->{check_name}; # The check name is in here
+    my $chip = $config->{chip};
+    my $sensor = $config->{sensor};
+
+    my $output = run_command("sysctl -n hw.sensors.$chip.$sensor");
+    $output =~ m/(-?\d+\.?\d*)\s+\S+/;
+    my $value = $1;
+
+    return {
+        "check_name" => [$self->{check_name}, "s"],
+        "chip" => [$config->{chip}, "s"],
+        "sensor" => [$config->{sensor}, "s"],
+        "value" => [$value, "n"]
+    };
+};
+
+1;
