427f32222a6808eceabbfcb459a387370bcb3d37
Mark Harrison <mark@mivok.net>
2011-03-14 16:48:34 -0400

    Remove svn revision checking from resmon1 - we're using git for distribution now

--- lib/Resmon/Module/RESMON.pm
+++ lib/Resmon/Module/RESMON.pm
@@ -6,7 +6,7 @@ use vars qw/@ISA/;
 use Sys::Hostname;
 
 # A resmon 'health check' module
-# Currently just prints the hostname and svn revision in the output, always
+# Currently just prints the hostname in the output, always
 # returning OK. In future it will check for a broken config and broken
 # modules, reporting that it is running with old modules/an old config.
 
@@ -20,25 +20,6 @@ sub handler {
     my $config = $main::config;
     my $configstatus = $config->{'configstatus'} || "";
     my $modstatus = $config->{'modstatus'} || "";
-
-    ## Current revision
-    # Find location of subversion binary
-    my $svn = 'svn';
-    for my $path (qw(/usr/local/bin /opt/omni/bin /opt/csw/bin)) {
-        if (-x "$path/svn") {
-            $svn = "$path/svn";
-            last;
-        }
-    }
-    my $output = cache_command("$svn info $resmon_dir 2>&1", 120);
-    my $revision = "svn revision unknown";
-    for (split(/\n/, $output)) {
-        if (/^Revision:\s*(\d*)$/) { $revision = "r$1"; }
-        if (/^svn: (.*) is not a working copy$/) {
-            $revision = "not a working copy";
-        }
-    }
-
 
     # The hostname command croaks (dies) if it fails, hence the eval
     my $hostname = eval { hostname } || "Unknown";
@@ -55,13 +36,12 @@ sub handler {
     }
 
     # Set 'config' variables so it shows up in the xml output
-    $arg->{'revision'} = $revision;
     $arg->{'hostname'} = $hostname;
     $arg->{'configstatus'} = $configstatus || "OK";
     $arg->{'modstatus'} = $modstatus || "OK";
 
     return $status, {
-        "message" => ["$hostname $statusmsg ($revision)", "s"]
+        "message" => ["$hostname $statusmsg", "s"]
     };
 }
 
